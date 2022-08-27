757f4286b634ca1af4041f4507f6f4601296aa6e
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2022-08-26 20:44:56 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+console.log("Hello");
+
+// let done = false;
+//
+// while(!done){
+//     /// comment
+// }
+//
+// console.log("I am done");
+//
+// /**
+// *** variables
+// *** loops
+// *** conditionals
+// *** functions
+// *** objects
+// *** arrays
+// *** callbacks
+// ****/
+
+
+// console.log("Loading main.js");
+
+//bringing in the top level function provided by the express framework
+let express = require('express');
+
+// this is creating our application object
+let app = express();
+
+// this is creating a number for my PORT
+let PORT = 8080;
+
+//for any get request, where the route is "/hello",
+//send the string "Sup Biotch" on the response
+app.get('/hello', function(request, response){
+    response.send("Sup Biotch!")
+})
+
+//this route should return the string of the form
+// 'Hello There :name"
+app.get('/hello/:name', function(request, response){
+    let name = request.params.name;
+    let message = (`Hello there ${name}`)
+    response.send(message);
+})
+
+//write a GET route definition that will do the following:
+
+// request url = /bye?name=mike  => 'see ya later mike';
+// request url = /bye?name=bob => 'see ya later bob';
+// request url = /bye => "see ya later"
+
+// hint you get the query parameters using : request.query.name
+
+// query
+app.get("/bye", function(request, response){
+    let name = request.query.name;
+    let age = request.query.age;
+    let location = request.query.location;
+
+    if (name) {
+        response.send( `see ya later ${name}`)
+    } else if (age) {
+        response.send(`your age is: ${age}`)
+    } else if (location) {
+        response.send(`your location is: ${location}`);
+    } else {
+        response.send(`see ya later`);
+    }
+})
+
+// params
+app.get('/bye/:name', function(request, response){
+    let value = request.params.name;
+    let message = (`see ya later: ${value} `)
+    response.send(message);
+
+})
+
+
+
+// This is my function that the app is listening to
+// (this is a callback function) - how to do the instructions
+// - basically a function within a function
+app.listen(PORT, function(){
+    console.log('Application started and listening on port', PORT);
+})
+
+
+//request.query.name
+//request.query.id
+
+
+
+
